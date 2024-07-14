634aebf32e8a3fdc6fb7fbebe89bcf4356f2fe00
Sujal Kamanna <sujalkamana2003@gmail.com>
2024-07-14 20:09:24 +0530

    update 1 file and create 1 file

--- arrays/arrays4.py
+++ arrays/arrays4.py
@@ -6,4 +6,4 @@ arr1 = np.array([1,2,3,4,5,6,7,8,])
 def traversal(array):
     for i in array:
         print(i)
-traversal(arr1)        
+traversal(arr1)
---
+++ arrays/arrays5.py
@@ -0,0 +1,18 @@
+# 1 create an array and traverse
+
+# 2 access individual elements through indexes
+# 3 append any value to array using append() method
+# 4 Insert value into array using insert() method
+# 5 Extend python array using extend() method
+# 6 add items from list into array using fromlist() method
+# 7 remove any element using remove() method
+# 8 remove last array element using pop() method
+# 9 fetch any element through its index using index() method
+# 10 reverse a python array using reverse() method
+# 11 get array buffer info through buffer_info() method
+# 12 check for no of occurance of element using count method
+# 13 remove all occurances of element in array using remove() method
+# 14 convert array to string using tostring() method
+# 15 convert array to list using fromstrinf() method
+# 16 slice elements from array
+# 17 append string to char array using fromstring() method
