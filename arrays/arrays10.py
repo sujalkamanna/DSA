355281e7682a15b73ec1baf6c6223b26e4357c85
Sujal Kamanna <sujalkamana2003@gmail.com>
2024-07-15 01:26:59 +0530

    feat: create arrays10.py

---
+++ arrays/arrays10.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+arr = np.array([
+    [1,2,3],
+    [2,3,4],
+    [7,8,9],
+])
+
+
+for i in range(len(arr)):
+    for j in range(len(arr[i])):
+        if arr[i][j]== 1:
+            print("the array is at",i,"row at",j,"column")
