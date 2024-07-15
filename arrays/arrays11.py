bbcf2e2518e089cb513593b237b33459421b17fb
Sujal Kamanna <sujalkamana2003@gmail.com>
2024-07-15 20:43:34 +0530

    "Modified array elements and added a new row."

--- arrays/arrays11.py
+++ arrays/arrays11.py
@@ -2,9 +2,10 @@ import numpy as np
 import arrays as arr 
 
 arr1=np.array([
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
+    [1,2,3,4],
+    [4,5,6,10],
+    [7,8,9,11],
+    [12,13,14,15]
     ]
 )
 print(arr1)
