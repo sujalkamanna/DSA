c0258f35e0004d510dd9d1fd326b47da1dceb915
Sujal Kamanna <sujalkamana2003@gmail.com>
2024-07-17 00:15:06 +0530

    feat: create list1.py and list2.py

---
+++ lists/list1.py
@@ -0,0 +1,18 @@
+# list DS
+list_int = [1, 2, 3, 4, 5]
+print(list_int)
+
+list_str = ['a', 'b', 'c', 'd', 'e']
+print(list_str)
+
+list_float = [0.1, 0.2, 0.3, 0.4, 0.5]
+print(list_float)
+
+list_mixed = ["string", True, False, None,"boolean", 0.1, 0.2, "str int", "0.01"]
+print(list_mixed)
+
+list_nested = [1, 2, 3, 4, ["hello world"], True, False]
+print(list_nested)
+
+list_nested1 = [1, 2, 3, 4, ["hello world",["python"]], True, False]
+print(list_nested1)
