dc86ee1b0f57ddf15d6ac6f88059f13c20759ff5
Sujal Kamanna <sujalkamana2003@gmail.com>
2024-07-17 19:41:12 +0530

    feat: create arrays15.py

---
+++ arrays/arrays15.py
@@ -0,0 +1,18 @@
+# finding element in array 
+
+def finf(arr, key):
+    for i in range(len(arr1)):
+        if arr[i] == key:
+            return i
+    return -1
+
+arr1 = [12, 34, 10, 6, 40]
+n = len(arr1)
+key = 34
+
+
+index = finf(arr1  key)
+if index != -1:
+    print("Element found at position: " + str(index + 1))  # +1 for 1-based index
+else:
+    print("Element not found")
