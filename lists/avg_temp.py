c6ea9192a06f31fc58c74d375e6aa534b4c555e8
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-19 00:39:31 +0530

    "Modified avg_temp.py to store temperature data in a list and added functionality to count days above average temperature."

--- lists/avg_temp.py
+++ lists/avg_temp.py
@@ -3,10 +3,18 @@
 num_days = int(input("How many no of days:"))
 
 temp = 0
-
-for i in range(1,num_days+1):
-    next_day  = int(input("day" +str(i)+ "'s high temp:"))
+data = []
+for i in range(0,num_days):
+    next_day  = int(input("day" +str(i+1)+ "'s high temp:"))
+    data.append(next_day)
     temp = temp + next_day
+
 
 avg = round(temp/num_days,2)
 print("Avg =" , str(avg))
+
+above = 0
+for i in data:
+    if i > avg:
+        above +=1
+print("Days above average temperature:", above)
---
+++ lists/list7.py
@@ -0,0 +1,25 @@
+a = 'hello'
+b = 'world'
+
+c = list(a) , list(b)
+d = list(a) + list(b)
+print(c)
+print(d)
+
+e = "hello how are you"
+f = e.split()
+print(f)
+
+delimeter = '-'
+g = 'hello-how-are-you'
+h = g.split(delimeter)
+print(h)
+
+#join function
+
+i = ['hello', 'how', 'are', 'you']
+j = " ".join(i)
+print(j)
+
+i = i+[10]
+print(i)
