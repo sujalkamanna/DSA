72bc780d4a965d4d8bf77a3a20410eb0353d6d02
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-20 01:13:49 +0530

    update 1 file and delete 201 files

--- dictionary/prog1.py
+++ dictionary/prog1.py
@@ -3,5 +3,5 @@ import os
 
 
 for i in range(0,100):
-    with open(f"file{i}.py","w") as f:
-        f.write("print('hello world')")
+    with open(f"file{i}.py","w+") as asyn:
+        asyn.write("print('')")
