4695eb610f97869337ba95ac0bb91c47bb12ae48
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-21 02:02:52 +0530

    added tuple and related files

---
+++ tuples/tuples1.py
@@ -0,0 +1,17 @@
+a = ()
+print(type(a))
+
+b = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(a)
+
+c = tuple('a')
+# c = ('a')
+print(c)
+print(type(c))
+
+print(b[0:])
+print(b[0:3])
+
+print(b[0:9:2])
+
+print(b[::-1])
