10ded23afb61d45eade9ba80a2a72ee93a7d34e4
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-22 00:53:41 +0530

    "Added two Python files: tuples2.py with tuple traversal examples and tuples3.py with tuple searching examples."

---
+++ tuples/tuples2.py
@@ -0,0 +1,10 @@
+#traversing in tuple
+
+tup = (1,2,3,4,5,6,7,8,9,)
+
+for  i in tup:
+    print(i)
+
+for j in range(len(tup)):
+    print(j)    
+
---
+++ tuples/tuples3.py
@@ -0,0 +1,17 @@
+#searching in tuple
+
+tup = (1,2,3,4,5,6,7,8,9,)
+
+print(1 in tup)
+if 1 in tup:
+    print(1)
+
+
+#linear search
+def linear(tup , element):
+    for i in range(len(tup)):
+        if tup[i] == element:
+            return i
+    return -1         
+
+print(linear(tup , 5))
