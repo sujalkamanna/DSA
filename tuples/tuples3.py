ce62b14c1e02d8724aeb53d8e80cb8974844d294
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-22 00:58:19 +0530

    "Updated tuples3.py with minor formatting changes and added tuples4.py with tuple operations"

--- tuples/tuples3.py
+++ tuples/tuples3.py
@@ -1,17 +1,18 @@
-#searching in tuple
+# searching in tuple
 
-tup = (1,2,3,4,5,6,7,8,9,)
+tup = (1, 2, 3, 4, 5, 6, 7, 8, 9,)
 
 print(1 in tup)
 if 1 in tup:
     print(1)
 
 
-#linear search
-def linear(tup , element):
+# linear search
+def linear(tup, element):
     for i in range(len(tup)):
         if tup[i] == element:
             return i
-    return -1         
+    return -1
 
-print(linear(tup , 5))
+
+print(linear(tup, 5))
---
+++ tuples/tuples4.py
@@ -0,0 +1,9 @@
+# tuple opeartions
+
+tup1 = (1, 2, 3, 4, 5)
+
+tup2 = (1, 2, 3, 47, 7)
+
+print(tup1+tup2)
+
+print(sorted(tup2))
