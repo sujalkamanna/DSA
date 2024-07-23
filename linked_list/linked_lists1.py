c0d325e11cfac3449b418bb78281168753ca800a
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-24 01:04:01 +0530

    "Refactor linked list implementation: rename class to Node, add Singly_Linked_List class, and create instances with nodes"

--- linked_list/linked_lists1.py
+++ linked_list/linked_lists1.py
@@ -1,6 +1,25 @@
 # creation of linked list
 
-class node():
-    def __init__(self,value = None) :
-        self.value = None
+class Node():
+    def __init__(self, value=None):
+        self.value = value
         self.next = None
+
+
+class ssl():
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+
+lst = ssl()
+node1 = Node(1)
+node2 = Node(2)
+
+lst.head = node1
+lst.head.next = node2
+lst.tail = node2
+
+print(lst.head.value)
+print(lst.tail.value)
+
+
---
+++ linked_list/linked_lists2.py
@@ -0,0 +1,25 @@
+class Singly_Linked_List():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+linked_list = Singly_Linked_List()
+
+obj1 = Node(1)
+obj2 = Node(2)
+obj3 = Node(3)
+
+linked_list.head = obj1
+linked_list.head.next = obj2
+linked_list.head.next.next = obj3
+linked_list.tail = obj2
+
+print(linked_list.head.value)
+print(linked_list.tail.value)
