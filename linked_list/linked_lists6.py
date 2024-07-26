ef14fed3c3b207f815d5ad36f3018fc87cd9fa37
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-26 17:43:07 +0530

    "Added decorator function and applied it to hello function"

---
+++ linked_list/linked_lists6.py
@@ -0,0 +1,12 @@
+def function(fun1):
+    def dec1():
+        print("*"*10)
+        fun1()
+        print("*"*10)
+    return dec1        
+
+def hello():
+    print("hello")
+
+x = function(hello)
+x()
