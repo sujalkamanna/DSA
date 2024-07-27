97a41d5ef83c277e404a8b1289ad4197bc1d3c03
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-28 00:54:04 +0530

    "Replaced infinite loop with input validation with implementation of a linked list with three nodes and a print method."

--- linked_list/linked_lists5.py
+++ linked_list/linked_lists5.py
@@ -1,11 +1,35 @@
-while True:
-    try:
-        a = float(input("Enter a number: "))
-        b = float(input("Enter another number: "))
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-        result = a / b
-        print(f"Result: {result}")
 
-    except ZeroDivisionError as e:
-        print(f"Error: {e}")
-        print("Please enter a non-zero denominator.")
+class linked_list():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def print_linked_list(self):
+        current_node = self.head
+        while current_node is not None:
+                print(current_node.data,end = "-->")
+                current_node = current_node.next
+        print("None")                               
+
+ll = linked_list()
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+
+ll.head = node1
+ll.head.next = node2
+ll.head.next.next = node3
+ll.tail = node3
+
+print(ll.head.data)
+print(ll.head.next.data)
+print(ll.head.next.next.data)
+print(ll.tail.data)
+
+ll.print_linked_list()
