e9c186ce7312a44645de3f32ceb24814af7c55df
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-28 01:01:27 +0530

    update linked_lists6.py

--- linked_list/linked_lists6.py
+++ linked_list/linked_lists6.py
@@ -1,15 +1,29 @@
-def function(fun1):
-    def dec1():
-        print("*"*10)
-        fun1()
-        print("*"*10)
-    return dec1        
+class Node():
+    def __init__(self,data) -> None:
+        self.data = data 
+        self.next = None
 
-def hello():
-    print("hello")
+class linkeed_list():
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
 
-x = function(hello)
+    def print_linked_list(self):
+        current_node = self.head 
+        while current_node is not None:
+            print(current_node.data,end=" --> ")
+            current_node = current_node.next
+        print("None")
 
+ll = linkeed_list()
 
-def decorator():
-    pass
+node1 = Node('A')
+node2 = Node('B')
+node3 = Node('C')
+
+ll.head = node1
+ll.head.next = node2
+ll.head.next.next = node3
+ll.tail = node3
+
+ll.print_linked_list()
