5781466550f90b09f3d4402cef309934b967adbe
Sujal Kamanna <sujalkamanna2003@gmail.com>
2024-07-31 00:14:30 +0530

    update linked_lists10.py

--- linked_list/linked_lists10.py
+++ linked_list/linked_lists10.py
@@ -1,21 +1,31 @@
-#inserting in between the nodes
+# inserting in between the nodes
 
 class Node():
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+
 
 class linkedlist():
     def __init__(self):
         self.head = None
         self.tail = None
 
+    def insert_after_node(self, prev_node, data):
+        if not prev_node:
+            print("Prev node is not in list!")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
     def printlist(self):
         current_node = self.head
         while current_node is not None:
-            print(current_node.data,end="-->") 
+            print(current_node.data, end="-->")
             current_node = current_node.next
         print("None")
+
 
 node1 = Node(1)
 node2 = Node(2)
@@ -23,8 +33,9 @@ node3 = Node(3)
 
 ll = linkedlist()
 ll.head = node1
-ll.head.next = node2 
+ll.head.next = node2
 ll.head.next.next = node3
 ll.tail = node3
 
+ll.insert_after_node(ll.head.next, 10)
 ll.printlist()
